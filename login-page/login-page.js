2e04eff097a26fc686f900dc40060ffc49a7af97
5huv0 <md88748874@gmail.com>
2024-03-05 00:29:47 +0600

    login page completed

--- login-page/login-page.js
+++ login-page/login-page.js
@@ -1,2 +1,11 @@
-document.getElementById('email').innerHTML;
-document.getElementById('pass').innerHTML;
+function check(){
+    let email = document.getElementById('email').value;
+    let pass = document.getElementById('pass').value;
+
+    if(email == 'shuvo88745@gmail.com' && pass == "123456"){
+        window.location.href='/bank/index.html';
+    }
+    else{
+        alert("Email or Password is not VALID!!!!");
+    }
+}
