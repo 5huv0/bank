8df47110a2d0ba8f70bb7d3e0d5232997026e2cb
5huv0 <md88748874@gmail.com>
2024-03-05 23:59:41 +0600

    adding js

---
+++ script.js
@@ -0,0 +1,12 @@
+//this function is for deposite button
+function addToDeposite(){
+    let v = '';
+    document.getElementById('deposite').innerHTML += v;
+}
+
+
+//this functioin is for withdraw button
+function addToWithdraw(){
+    let y = '';
+    document.getElementById('deposite').innerHTML += y;
+}
